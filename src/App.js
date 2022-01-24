e0dda8c42c6ba2122a46b770ac607546fbb44401
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-01-25 00:00:26 +0530

    removed test notes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,7 @@ import Search from './components/Search';
 import { nanoid } from 'nanoid';
 import Header from './components/Header';
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: 'this is my first note!',
-      date: '24/01/2022',
-    },
-    {
-      id: nanoid(),
-      text: 'this is my second note!',
-      date: '26/01/2022',
-    },
-    {
-      id: nanoid(),
-      text: 'this is my third note!',
-      date: '21/01/2022',
-    },
-  ]);
+  const [notes, setNotes] = useState([]);
 
   const [searchText, setSearchText] = useState('');
   const [isDarkMode, setIsDarkMode] = useState(false);
