46baea2740a480d1fd37e513d778f21ce5f60a36
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-01-24 15:55:59 +0530

    added note and noteslist component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import NotesList from './components/NotesList';
 function App() {
-  return <div>hello world!</div>;
+  return (
+    <div className="container">
+      <NotesList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MdDeleteForever } from 'react-icons/md';
+function Note() {
+  return (
+    <div className="note">
+      <span>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi ducimus
+        libero animi iste esse eum.
+      </span>
+      <div className="note-footer">
+        <small>24/01/22</small>
+        <MdDeleteForever className="delete-icon" size="1.3em" />
+      </div>
+    </div>
+  );
+}
+
+export default Note;
